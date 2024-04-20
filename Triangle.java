aae0d7958ce4a19a70f45255c99668b19d0ce6e1
saikarthick2003 <saikarthickm2020ai@gmail.com>
2024-04-20 11:45:47 +0530

    Triangle.java added

---
+++ Triangle.java
@@ -0,0 +1,18 @@
+package Patterns;
+public class Triangle {
+    public static void main(String args[]) {
+        int n = 5;
+        for(int i = 0; i < 5 ; i++)
+        {
+          for(int j = 0;j<n-i-1;j++)
+              System.out.print("   ");
+          for(int k = 0;k<2*i+1;k++)
+              System.out.print(" * ");
+          for(int l = 0;l<n-i-1;l++)
+              System.out.print("   ");
+          
+         System.out.println();
+        }
+        
+    }
+}
